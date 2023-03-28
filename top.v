59d64113221051eb0f65a4904011f72b705e61fd
Tom Verbeure <hombre+github.com@gmail.com>
2023-03-27 20:19:31 -0700

    DFF map works.

--- top.v
+++ top.v
@@ -1,16 +1,22 @@
 
 module top(
     input wire          clk,
-    input wire [15:0]   a,
-    input wire [15:0]   b,
+    input wire [7:0]   a,
+    input wire [7:0]   b,
     input wire          ena,
-    output wire [15:0]  sum
+    output wire [7:0]  sum
     );
+
+    reg [7:0] a_r;
+    reg [7:0] b_r;
 
     always @(posedge clk) begin
         if (ena) begin
-            sum <= a + b;
+            sum <= a_r + b_r;
         end
+
+        a_r <= a;
+        b_r <= b;
     end
 
 endmodule
